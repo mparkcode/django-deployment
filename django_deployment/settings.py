71363a7ae7bd36118d9783bf48d2173b092edfc2
Michael Park <mparkcode@gmail.com>
2020-09-21 15:37:37 +0000

    DEBUG setup

--- django_deployment/settings.py
+++ django_deployment/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['mpark-django-deployment.herokuapp.com', 'localhost']
 
